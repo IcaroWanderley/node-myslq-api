c0a931448eb632de83ab3eb0a2677b016a91b23c
IcaroWanderley <86797083+IcaroWanderley@users.noreply.github.com>
2024-09-05 00:52:07 -0300

    Implementação da validação e autenticação

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,10 @@ const express = require('express');
 const postsController = require('../controllers/post.controller');
 
 const router = express.Router();
-router.get("/", postsController.index);
 
+router.post("/", postsController.save);
+router.get("/", postsController.index);
+router.get("/:id", postsController.show);
+router.patch("/:id", postsController.update);
+router.delete("/:id", postsController.destroy);
 module.exports = router;
